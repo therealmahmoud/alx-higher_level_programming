b3be3f81086267b07d31c994e71270a9d2c5db09
therealmahmoud <moha91875460@gmail.com>
2023-11-29 00:21:40 +0000

    UPDATED

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,10 +2,10 @@
 def fizzbuzz():
     for n in range(1, 101):
         if (n // 5) * 5 == n and (n //3) * 3 == n:
-            print("fizzbuzz", end=" ")
+            print("FizzBuzz", end=" ")
         elif (n // 3) * 3 == n:
-            print("fizz", end=" ")
+            print("Fizz", end=" ")
         elif (n // 5) *5 == n:
-            print("buzz", end=" ")
+            print("Buzz", end=" ")
         else:
             print(n, end=" ")
