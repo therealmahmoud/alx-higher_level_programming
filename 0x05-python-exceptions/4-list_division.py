af7f0d5b82dcb32f240203a5ec4902766d5293f8
therealmahmoud <moha91875460@gmail.com>
2023-12-18 22:44:40 +0000

    UPDATED

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def list_resision(my_list_1, my_list_2, list_length):
+def list_division(my_list_1, my_list_2, list_length):
     new_lis = []
     for i in range(0, list_length):
         try:
