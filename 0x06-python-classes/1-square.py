cd01eb6409afe5cb2228106df90c0e9b31bc769c
therealmahmoud <moha91875460@gmail.com>
2023-12-19 22:53:52 +0000

    UPDATED

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""
+Define a class square.
+"""
+class Square:
+    """
+    A class which the size attribute is private.
+    """
+    def __init__(self, size):
+        self.__size = size
