a7165bcb0be457c7218f611f0463f771f2d447c2
therealmahmoud <moha91875460@gmail.com>
2023-12-20 00:54:52 +0000

    UPDATED

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+
+"""
+Define a class square.
+"""
+
+
+class Square:
+    """
+    A class which the size attribute is private.
+    """
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise ValueError("size must be an integer")
+        if (size < 0):
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise ValueError("size must be an integer")
+        self.__size = value
+
+    def my_print(self):
+        for i in range(self.__size):
+            for j in range(self.__size):
+                print("#", end="")
+            print("")
+        if (self.__size == 0):
+            print("")
