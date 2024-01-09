cf2da26a68e7dca1dc08daefa9358242cc422042
therealmahmoud <moha91875460@gmail.com>
2024-01-09 01:14:27 +0000

    UPDATED

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+Defines a Lookup function.
+"""
+
+
+def lookup(obj):
+    """ lookup function that return list object. """
+    return dir(obj)
---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" defines BaseGeometry class """
+
+
+class BaseGeometry:
+    """Represent base geometry."""
+    pass
