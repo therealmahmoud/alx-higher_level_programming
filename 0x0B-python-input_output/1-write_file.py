1d0887b6d1bad0c39cf6060cceaabdf865c70ace
therealmahmoud <moha91875460@gmail.com>
2024-01-10 00:28:44 +0000

    UPDATED

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -8,5 +8,5 @@ of characters written
 
 def write_file(filename="", text=""):
     """ write a string to the content of the file. """
-    with open(filename, encoding="utf-8") as f:
-        return len(text)
+    with open(filename, 'w', encoding="utf-8") as f:
+        return f.write(text)
