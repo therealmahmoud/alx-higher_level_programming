8439448211b45ff27b65a2e302c6884a25b1fd45
therealmahmoud <moha91875460@gmail.com>
2024-01-17 20:14:11 +0000

    UPDATED

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -145,3 +145,14 @@ class Rectangle(Base):
         """
         return f"[Rectangle] ({self.id}) {self.x}/{self.y}\
  - {self.width}/{self.height}"
+
+    def update(self, *args):
+        """ Updates arguments in class. """
+        try:
+            self.id = args[0]
+            self.width = args[1]
+            self.height = args[2]
+            self.x = args[3]
+            self.y = args[4]
+        except IndexError:
+            pass
