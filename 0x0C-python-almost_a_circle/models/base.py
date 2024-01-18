6f4adbf6d490c6753eefe144c93f2c8fa2afec9f
therealmahmoud <moha91875460@gmail.com>
2024-01-18 01:03:41 +0000

    UPDATED

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -82,11 +82,11 @@ class Base:
         '''
         file_name = cls.__name__ + ".json"
 
-        try:
+        if file_name is None:
+            return []
+        else:
             with open(file_name, encoding="UTF8") as fd:
                 content = cls.from_json_string(fd.read())
-        except:
-            return []
 
         instances = []
 
