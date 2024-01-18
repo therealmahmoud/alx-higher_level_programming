42270a8ce0714704c74e77849b94bf382f794642
therealmahmoud <moha91875460@gmail.com>
2024-01-18 00:01:21 +0000

    UPDATED

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """ A class which can be accessed. """
+import json
 
 
 class Base:
@@ -24,3 +25,10 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        """ JSON string representation of list_dictionaries. """
+        if len(list_dictionaries) == 0 or list_dictionaries == None:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
