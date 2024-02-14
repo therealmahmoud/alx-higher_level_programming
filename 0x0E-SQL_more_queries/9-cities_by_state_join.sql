37044c5ee4cf657defae041eeb1962a03f4b4a59
therealmahmoud <moha91875460@gmail.com>
2024-02-14 21:56:23 +0000

    UPDATED

---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,6 @@
+-- a script that lists all cities contained in the database hbtn_0d_usa.
+SELECT cities.id, cities.name, states.name
+FROM city INNER JOIN states
+ON cities.id = states.state_id
+ORDER BY cities.id ASC;
+
