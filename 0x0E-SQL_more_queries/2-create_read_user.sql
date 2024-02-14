3849fb1d859e1b91a3e514194b4d2f811df56f10
therealmahmoud <moha91875460@gmail.com>
2024-02-14 20:18:54 +0000

    UPDATED

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,6 +1,7 @@
 -- a script that creates the database hbtn_0d_2 and the user user_0d_2.
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2
-GRANT SELECT
-ON .*
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT
+ON hbtn_0d_2.*
+to 'user_0d_2'@'localhost'
 
