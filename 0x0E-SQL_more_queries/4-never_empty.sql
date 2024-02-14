01c2e8eaa67d24d37e1420c8ca6bfff46e43a5f9
therealmahmoud <moha91875460@gmail.com>
2024-02-14 20:31:19 +0000

    UPDATED

--- 0x0E-SQL_more_queries/4-never_empty.sql
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -1,2 +1,2 @@
 -- a script that creates the table id_not_null on your MySQL server.
-CREATE TABLE IF NOT EXISTS (id INT=1, name VARCHAR(256));
+CREATE TABLE IF NOT EXISTS (id INT(1), name VARCHAR(256));
