d13b460a9a5d4af0cb4deb4fabb02b10032e6ae3
therealmahmoud~ <moha91875460@gmail.com>
2024-03-13 02:47:52 +0200

    updated

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,10 +1,6 @@
 #!/usr/bin/node
 const square = require('./5-square');
 class Square extends square {
-  constructor (size) {
-    super(size, size);
-  }
-
   charPrint (c) {
     if (c === undefined) {
       for (let i = 0; i < this.height; i++) {
