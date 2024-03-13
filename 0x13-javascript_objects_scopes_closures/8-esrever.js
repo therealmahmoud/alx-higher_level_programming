1ef78fc51e70ae150c3bf3fa6df6658f3a449618
therealmahmoud~ <moha91875460@gmail.com>
2024-03-13 03:14:54 +0200

    updated

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+exports.esrever = function (list) {
+  const c = [];
+  let j = 0;
+  for (let i = list.length - 1; i !== -1; i--) {
+    c[j] = list[i];
+    j++;
+  }
+  return c;
+};
