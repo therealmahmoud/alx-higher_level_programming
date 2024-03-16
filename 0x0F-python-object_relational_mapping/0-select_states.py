6394a29d578f48314669201884fb45e64e05e240
therealmahmoud~ <moha91875460@gmail.com>
2024-03-16 23:56:22 +0200

    updated

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""  lists all states from the database hbtn_0e_0_usa """
+import MySQLdb
+import sys
+
+
+conn = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                  passwd=sys.argv[2], db=sys.argv[3], port=3306)
+cur = conn.cursor()
+cur.execute("SELECT * FROM states")
+
+for row in cur.fetchall():
+    print (row)
