3ac2286b0e9bcd99180394d6fb1d6eb3f8b67b15
therealmahmoud~ <moha91875460@gmail.com>
2024-03-29 01:57:47 +0200

    updated

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 from urllib import request
+"""A script that fetches https://alx-intranet.hbtn.io/status
+"""
 
 with request.urlopen("https://alx-intranet.hbtn.io/status") as resp:
     response = resp.read()
