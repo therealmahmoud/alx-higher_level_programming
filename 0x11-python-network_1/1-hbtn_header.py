66b4d95f782307ab3a8e4fef78fe6a41526fab40
therealmahmoud~ <moha91875460@gmail.com>
2024-03-29 02:28:54 +0200

    updated

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -5,5 +5,6 @@ sends a request to the URL and displays the value
 from urllib import request
 import sys
 
-with request.urlopen(sys.argv[1]) as resp:
+req = request.Request(sys.argv[1])
+with request.urlopen(req) as resp:
     print(dict(resp.headers).get("X-Request-Id"))
